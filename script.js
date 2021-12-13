35256e5d18fac56e1770a6a2b4e847bcae24c623
Nicole Thompson <Nicole@MacBook-Pro-2.local>
2021-12-13 12:51:41 -0800

    creating a button with a click event

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+
+// console.log($.ajax('https://api.adviceslip.com/advice'))
 //variables
-const URL = 'https://api.adviceslip.com/advice';
+// const URL = 'https://api.adviceslip.com/advice';
 
 //element reference
 const $id = $('#slip.id');
 const $advice = $('#slip.advice');
+const $form = $('form')
+const $input = $('input[type="button"]');
 
 //element listeners
+$form.on('click', handleClick);
+
+//functions
+function handleClick(evt){
+    console.log(evt);
+    evt.preventDefault();
+
+    $.ajax('https://api.adviceslip.com/advice').then(function(advice){
+        render(advice);
+    });
+}
+
+function render(adviceSlip){
+    $('main').html(`
+    <p>${adviceSlip.advice}</p>
+    `);
+}
+
